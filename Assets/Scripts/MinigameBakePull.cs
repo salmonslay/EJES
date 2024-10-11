c471e32e88f82ae1a8ed12cc22c6dc600a6b6642
Ebby00 <eliasben00@gmail.com>
2024-10-11 12:20:10 +0200

    added the buttons for the input options to my scenes

--- Assets/Scripts/MinigameBakePull.cs
+++ Assets/Scripts/MinigameBakePull.cs
@@ -82,7 +82,7 @@ public class MinigameBakePull : Minigame
 
             if(sliderPos >= rangeUpper)
             {
-                EndMinigame();
+                EndMinigame(false);
             }
         }
         
